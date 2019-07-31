9db9d485c1e3f601ad1c60f6a7060e7d2af2fb10
Craig <craig@paltechnologies.com>
2019-07-31 11:01:54 +0100

    Updated to remove na.rm parameter and change other parameter names to enhance readability

--- R/weighted.quantile.R
+++ R/weighted.quantile.R
@@ -1,26 +1,26 @@
 # Weighted quantile
-weighted.quantile <- function(x, w, probs=0.25, na.rm=TRUE) {
-  if(length(x) == 0 | length(w) == 0){
+weighted.quantile <- function(values, weights, probs=0.5) {
+  if(length(values) == 0 | length(weights) == 0){
     return(NaN)
   }
-  if(length(x) == 1 | length(w) == 1){
-    return(x)
+  if(length(values) == 1 | length(weights) == 1){
+    return(values)
   }
-  value_table <- data.frame(x,w)
-  value_table <- value_table[order(value_table$x),]
-  value_table$rw <- cumsum(value_table$w)
-  pos <- sum(value_table$w) * probs
+  value_table <- data.frame(values,weights)
+  value_table <- value_table[order(value_table$values),]
+  value_table$rw <- cumsum(value_table$weights)
+  pos <- sum(value_table$weights) * probs
 
   if (length(which(value_table$rw == pos)) == 1){
-    return(value_table[which(value_table$rw == pos),]$x)
+    return(value_table[which(value_table$rw == pos),]$values)
   }
 
   if(pos < min(value_table$rw)){
-    return(value_table[1,]$x)
+    return(value_table[1,]$values)
   }
 
   if(pos > max(value_table$rw)){
-    return(value_table[nrow(value_table),]$x)
+    return(value_table[nrow(value_table),]$values)
   }
 
   lower_median_pos <- max(which(value_table$rw <= pos))
@@ -31,11 +31,11 @@ weighted.quantile <- function(x, w, probs=0.25, na.rm=TRUE) {
   upper_proportion <- (pos - value_table[lower_median_pos,]$rw) /
     (value_table[upper_median_pos,]$rw - value_table[lower_median_pos,]$rw)
 
-  return(value_table[lower_median_pos,]$x * lower_proportion +
-           value_table[upper_median_pos,]$x * upper_proportion)
+  return(value_table[lower_median_pos,]$values * lower_proportion +
+           value_table[upper_median_pos,]$values * upper_proportion)
 }
 
 # Weighted median
-weighted.median <- function(x, w, na.rm=TRUE) {
-  unname(weighted.quantile(x, probs=0.5, w=w, na.rm=na.rm))
+weighted.median <- function(values, weights) {
+  unname(weighted.quantile(values, weights, probs=0.5))
 }
